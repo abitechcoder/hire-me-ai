4ba5367a18bba169f9df2705c603fe4a18bf9d84
abitechcoder <abiolaolalekan39@gmail.com>
2025-11-26 04:26:25 +0100

    renamed env variables name

--- src/conf/config.ts
+++ src/conf/config.ts
@@ -1,6 +1,6 @@
 const conf = {
-    appwriteUrl: String(process.env.NEXT_PUBLIC_APPWRITE_BASE_URL),
-    appwriteProjectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID),
+    appwriteUrl: String(process.env.APPWRITE_BASE_URL),
+    appwriteProjectId: String(process.env.APPWRITE_PROJECT_ID),
     airtableAPIKey: String(process.env.AIRTABLE_API_KEY),
     airtableBaseId: String(process.env.AIRTABLE_BASE_ID),
 }
